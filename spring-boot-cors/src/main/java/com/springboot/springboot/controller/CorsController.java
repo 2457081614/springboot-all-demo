d91158e1a239f04888b4e8d6a038e500b853fc37
杨柳 <yangliu@tiduyun.com>
2019-11-06 09:54:22 +0800

    添加跨域处理demo

---
+++ spring-boot-cors/src/main/java/com/springboot/springboot/controller/CorsController.java
@@ -0,0 +1,26 @@
+package com.springboot.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author by yangliu@tiduyun.com
+ * @Description TODO
+ */
+@Controller
+public class CorsController {
+
+    @RequestMapping(value = "index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("hello")
+    @ResponseBody
+    @CrossOrigin(value = "*")
+    public String hello() {
+        return "hello";
+    }
+}
