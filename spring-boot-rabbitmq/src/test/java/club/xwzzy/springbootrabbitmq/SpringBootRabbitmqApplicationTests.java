5c156d39a9996f1c5a33afb0b8322cca82a5221b
杨柳 <yangliu@tiduyun.com>
2019-11-26 14:19:42 +0800

    添加邮件处理模块

---
+++ spring-boot-rabbitmq/src/test/java/club/xwzzy/springbootrabbitmq/SpringBootRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package club.xwzzy.springbootrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
